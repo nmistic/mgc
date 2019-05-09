094decf9cb67837c2d0c6291b1babdccd04aaa86
nmistic <nihilistimistic@gmail.com>
2019-05-09 14:19:18 +0530

    preproc incomplete

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -13,3 +13,19 @@ N_MELS     = 96
 # number of samples between successive frames
 N_OVERLAP  = 256
 DURA       = 29.12
+
+
+def log_scale_melspectrogram(path, plot=False):
+    # load song signal as waveform and sampling rate from path
+    signal, sr = lb.load(path, sr=Fs)
+
+    n_sample = signal.shape[0]
+    n_sample_fit = int(DURA * Fs)\
+
+    # does the sample fit ?
+    if n_sample < n_sample_fit:
+        # if smaller, fill the remaining with zeros
+        signal = np.hstack((signal, np.zeros((int(DURA * Fs) - n_sample,))))
+    elif n_sample > n_sample_fit:
+        # if larger, take the middle part from (n_sample-n_sample_fit)/2 to (n_sample+n_sample_fit)/2
+        signal = signal[(n_sample-n_sample_fit)/2:(n_sample + n_sample_fit)/2]
